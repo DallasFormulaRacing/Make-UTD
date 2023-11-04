c3a752de13cd2a5f893dd75385b1236247bd4e59
bellh14 <haybell965@gmail.com>
2023-11-03 22:10:22 -0500

    inital from mui example

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,34 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontSize: {
+        "10xl": "10rem",
+    },
+    spacing: {
+        128: "32rem",
+        144: "36rem",
+        160: "40rem",
+        176: "44rem",
+        224: "56rem",
+        256: "64rem",
+        400: "100rem",
+    },
+    colors: {
+        "wh-0": "#FFFFFF",
+        "wh-10": "#F4F4F4",
+        "wh-50": "#FBFBFB",
+        "wh-75": "#F7F7F7",
+        "wh-100": "#C9C9C9",
+        "wh-300": "#939393",
+        "wh-500": "#595959",
+        "wh-900": "#0F0F0F",
+        "accent-red": "#EA9648",
+        "accent-orange": "#E87500",
+        "officer-orange": "#EE9740",
+        "accent-green": "#2D8B49",
+        "accent-yellow": "#EDCA2C",
+        "or-10": "#ff841c",
+    },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
