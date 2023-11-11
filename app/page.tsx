347cc21c742a66f342d168d479c25f2e67161372
bellh14 <haybell965@gmail.com>
2023-11-11 14:09:14 -0600

    added slightly more styling

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import About from "./(home)/About";
 
 export default function Home() {
     return (
-        <main className="">
+        <main className="bg-wh-100">
             <div className="relative h-[100svh] bg-[url('../public/temp_space_home.jpg')] bg-cover">
                 <Navbar />
             </div>
